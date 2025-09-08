2dabc45f021daffd54048f2c6f4f78febdc92a7d
Wilson Suarez <wsuarez874@unab.edu.co>
2025-09-08 18:57:41 -0500

    Tercera Habilidad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
             margin-bottom: 1rem;
             font-size: 1.8rem;
             text-align: center;
-            
+
         }
 
         .habilidades {
@@ -263,10 +263,16 @@
 
     <!-- Sección de Habilidades -->
     <section class="seccion">
-        <h2>Habilidades</h2>
+        <h3>Habilidades</h3>
         <ul class="habilidades">
             <li>Administración y mantenimiento de equipos de cómputo</li>
             <li>Manejo de Git /GitHub y paquete de Office</li>
+            <li>Trabajo en equipo y resolución de problemas</li>
+            <h3>Conocimientos Técnicos</h3>
+            <ul class= "habilidades">
+                <li>Lenguajes: Python, PHP, Java, Kotlin, SQL y JavaScript</li>
+                <li>Frameworks: Laravel, React</li>
+            </ul>
         </ul>
     </section>
 
